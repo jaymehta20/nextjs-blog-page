8a292e56e6abdd626e6402a6742579b93ef8ed81
Jay Mehta <jake2001007@gmail.com>
2021-04-26 22:04:13 +0530

    reverted back to img tag

--- pages/[slug].js
+++ pages/[slug].js
@@ -71,7 +71,7 @@ const renderOptions = {
     [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
       // render the EMBEDDED_ASSET as you need
       return (
-        <Image
+        <img
           src={`https://${node.data.target.fields.file.url}`}
           height={node.data.target.fields.file.details.image.height}
           width={node.data.target.fields.file.details.image.width}
